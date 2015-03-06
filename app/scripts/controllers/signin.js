83fab7cc0ab59ed85503077c33802bdac742f04c
PanteraLX <samuel.heinzmann@bluewin.ch>
2015-03-06 17:03:33 +0100

    Login-funktion erweitert

--- app/scripts/controllers/signin.js
+++ app/scripts/controllers/signin.js
@@ -8,6 +8,19 @@
  * Controller of the webtrackApp
  */
 angular.module('webtrackApp')
-  .controller('SigninCtrl', function ($scope) {
-    $scope.hallo = 'hallo';
+  .controller('SigninCtrl', function ($scope, $firebaseSimpleLogin, $location) {
+    var ref = new Firebase('https://webtrack.firebaseio.com/')
+    var simpleLogin = $firebaseSimpleLogin(ref);
+
+    $scope.login = function() {
+      simpleLogin.$login('password', {
+        email: $scope.user.email,
+        password: $scope.user.password
+      }).then(function(user){
+        $location.path('/main');
+
+      })
+
+
+    }
   });
