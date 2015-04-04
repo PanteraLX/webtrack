86107a7a8c8b67a832c2fb258c50448dac822690
PanteraLX <samuel.heinzmann@bluewin.ch>
2015-04-04 22:22:46 +0200

    Add-Funktion in Firebase-DB hinzugefügt

--- app/scripts/controllers/add.js
+++ app/scripts/controllers/add.js
@@ -8,10 +8,25 @@
  * Controller of the webtrackApp
  */
 angular.module('webtrackApp')
-  .controller('AddCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('AddCtrl', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
+
+    var url = "https://webtrack.firebaseio.com/data_projects"
+    var ref = new Firebase(url);
+    var projects = $firebaseArray(ref);
+
+    $scope.projects = projects;
+
+    $scope.addProject = function() {
+      projects.$add({
+        projectId : Math.floor(Math.random() * 100),
+        projectName: $scope.projectName,
+        projectLeader: $scope.projectLeader,
+        projectLength: $scope.projectLength,
+        employees: $scope.employees,
+        date: Firebase.ServerValue.TIMESTAMP
+      }).then( function () {
+        $scope.projectName = '';
+      })
+    }
+
+  }]);
