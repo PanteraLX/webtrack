7859a243329ad3e9dd806c948268966c34cd3a37
PanteraLX <samuel.heinzmann@bluewin.ch>
2015-04-05 00:37:54 +0200

    SharedProperties Service hinzugefügt

---
+++ app/scripts/services/sharedproperties.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name webtrackApp.sharedProperties
+ * @description
+ * # sharedProperties
+ * Service in the webtrackApp.
+ */
+angular.module('webtrackApp')
+  .service('sharedProperties', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
