e4e550ba38dfa9c7416cbcbde32b5adce7101414
taarist <stefanie.richter@bluewin.ch>
2015-04-05 12:17:41 +0200

    Löschfunktion bei edit.js hinzugefügt

--- app/scripts/controllers/edit.js
+++ app/scripts/controllers/edit.js
@@ -10,4 +10,11 @@
 angular.module('webtrackApp')
   .controller('EditCtrl', ['$scope', 'sharedProperties', function ($scope, sharedProperties) {
     $scope.projectToEdit = sharedProperties.getObject();
+
+    $scope.removeSingleProject = function removeSingleProject(key) {
+      var box = window.confirm("Wollen sie das Pojekt wirklich löschen?")
+      if (box) {
+        projects.$remove(key);
+      }
+    };
   }]);
