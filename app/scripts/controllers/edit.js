fb5a244d161b61101e1149f9890c3f35896f5723
PanteraLX <samuel.heinzmann@bluewin.ch>
2015-05-20 22:37:37 +0200

    Edit-Funktion implementiert (funktioniert noch nicht ganz)

--- app/scripts/controllers/edit.js
+++ app/scripts/controllers/edit.js
@@ -8,13 +8,43 @@
  * Controller of the webtrackApp
  */
 angular.module('webtrackApp')
-  .controller('EditCtrl', ['$scope', 'sharedProperties', function ($scope, sharedProperties) {
-    $scope.projectToEdit = sharedProperties.getObject();
+  .controller('EditCtrl', ['$scope', '$firebaseArray','sharedProperties','$location',
+    function ($scope, $firebaseArray, sharedProperties, $location) {
 
-    $scope.removeSingleProject = function removeSingleProject(key) {
+    $scope.projectToEdit = sharedProperties.getObject();
+    $scope.projectKey = sharedProperties.getString();
+
+    $scope.projectName = $scope.projectToEdit.projectName;
+    $scope.projectLeader = $scope.projectToEdit.projectLeader;
+    $scope.projectLength = $scope.projectToEdit.projectLength;
+    $scope.employees = $scope.projectToEdit.employees;
+
+
+    var url = "https://webtrack.firebaseio.com/data_projects"
+    var ref = new Firebase(url);
+    var projects = $firebaseArray(ref);
+
+    $scope.projects = projects;
+
+    $scope.editProject = function() {
+      projects.$add({
+        projectId :  Math.floor(Math.random() * 100),
+        projectName: $scope.projectName,
+        projectLeader: $scope.projectLeader,
+        projectLength: $scope.projectLength,
+        employees: $scope.employees,
+        date: Firebase.ServerValue.TIMESTAMP
+      }).then( function () {
+        $location.path('/overview');
+      })
+    }
+
+    $scope.removeSingleProject = function removeSingleProject() {
       var box = window.confirm("Wollen sie das Projekt wirklich löschen?")
       if (box) {
-        $scope.projects.$remove(key);
+        $scope.projects.$remove($scope.projectKey);
       }
+      $location.path('/overview');
     };
+
   }]);
