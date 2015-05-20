ec10c69c74369c6f6e4b9422df1e3ac30f981f87
PanteraLX <samuel.heinzmann@bluewin.ch>
2015-05-20 23:23:26 +0200

    Logout-Funktion löscht erhaltenen Token aus Cookies

--- app/scripts/controllers/logout.js
+++ app/scripts/controllers/logout.js
@@ -8,10 +8,8 @@
  * Controller of the webtrackApp
  */
 angular.module('webtrackApp')
-  .controller('LogoutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('LogoutCtrl',['$scope', '$cookieStore', function ($scope, $cookieStore) {
+
+    $cookieStore.remove('token')
+
+  }]);
